1bf8512ec43c49c7857c929da426f60228aaf40d
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-23 11:51:02 -0300

    :construction: feat: componentes da page de home user

--- src/app/home-user/home-user.component.ts
+++ src/app/home-user/home-user.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeUserComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
