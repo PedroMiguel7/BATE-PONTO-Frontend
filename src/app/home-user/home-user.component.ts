d4655ac5101882a46a7a9bd0a86cd5f167e84a64
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-23 22:02:45 -0300

    :sparkles:feat: criando componente de ponto

--- src/app/home-user/home-user.component.ts
+++ src/app/home-user/home-user.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { USER } from './home-user.interface';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-home-user',
   templateUrl: './home-user.component.html',
   styleUrls: ['./home-user.component.scss']
 })
+
+
 export class HomeUserComponent implements OnInit {
 
-  constructor() { 
-    
-  }
+
+  constructor() 
+  {}
+
+
+  loading: boolean = false
 
   ngOnInit(): void {
   }
