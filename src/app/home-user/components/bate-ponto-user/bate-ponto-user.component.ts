a0ee20b4abcc9aafbf65e745cc5b3fbf550a9fb7
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-24 20:59:27 -0300

    :construction:feat: ponto criado, e imprimindo os pontos, mas não consumindo api ainda

--- src/app/home-user/components/bate-ponto-user/bate-ponto-user.component.ts
+++ src/app/home-user/components/bate-ponto-user/bate-ponto-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PONTO } from './bate-ponto-user.interface';
 
 @Component({
   selector: 'app-bate-ponto-user',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bate-ponto-user.component.scss']
 })
 export class BatePontoUserComponent implements OnInit {
+  ponto: PONTO = {
+    id: 3,
+    data: 'ter, 18 de maio de 2022',
+    hora: '08:10',
+    userID: 3,
+    status: 1
+  }
+  ponto2:  PONTO = {
+    id: 4,
+    data: 'ter, 18 de maio de 2022',
+    hora: '11:40',
+    userID: 3,
+    status: 2
+  }
+
+  pontos: Array<PONTO> = [this.ponto2, this.ponto];
 
   constructor() { }
 
---
+++ src/app/home-user/components/bate-ponto-user/bate-ponto-user.interface.ts
@@ -0,0 +1,7 @@
+export interface PONTO{
+    id: number;
+    data: string;
+    hora: string;
+    userID: number;
+    status: number;
+}
