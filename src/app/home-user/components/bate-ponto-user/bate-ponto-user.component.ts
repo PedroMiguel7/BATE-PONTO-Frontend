6e9e7d5a58baa64ec6c50cbfc01a1affe4b8a334
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-28 11:09:08 -0300

    :recycle: refactor: trocando interface do ponto e alterando a forma como é tratado

--- src/app/home-user/components/bate-ponto-user/bate-ponto-user.component.ts
+++ src/app/home-user/components/bate-ponto-user/bate-ponto-user.component.ts
@@ -12,21 +12,29 @@ export class BatePontoUserComponent implements OnInit {
   @Input() IDUSER!: any;
   ponto: PONTO = {
     id: 3,
-    data: 'ter, 18 de maio de 2022',
-    hora: '08:10',
     userID: 3,
-    status: 1
+    freqID: 2,
+    dataEntrada: '17/08/2009 08:00:00 -3:00',
+    dataSaida: '17/08/2009 11:50:03 -3:00',
   }
 
   ponto2:  PONTO = {
     id: 4,
-    data: 'ter, 18 de maio de 2022',
-    hora: '11:40',
     userID: 3,
-    status: 2
+    freqID: 3,
+    dataEntrada: '18/08/2009 08:05:10 -3:00',
+    dataSaida: '18/08/2009 11:54:23 -3:00',
   }
 
-  pontos: Array<PONTO> | any = [this.ponto2, this.ponto, this.ponto2, this.ponto, this.ponto2, this.ponto, this.ponto2, this.ponto, this.ponto2, this.ponto];
+  ponto3:  PONTO = {
+    id: 4,
+    userID: 3,
+    freqID: 3,
+    dataEntrada: '19/08/2009 08:10:10 -3:00',
+    dataSaida: "19/08/2009 11:59:44 -3:00",
+  }
+
+  pontos: Array<PONTO> | any = [this.ponto2, this.ponto, this.ponto3];
 
   constructor(private pontosService: PontosUserService) { }
 
