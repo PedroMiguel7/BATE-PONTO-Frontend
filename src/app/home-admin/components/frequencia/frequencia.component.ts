d843b8c8ae35685b2cdb3c93112af690b98e696d
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-09-28 10:53:48 -0300

    :sparkles: feat: Estruturação da Página e Componente da Tabela

---
+++ src/app/home-admin/components/frequencia/frequencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frequencia',
+  templateUrl: './frequencia.component.html',
+  styleUrls: ['./frequencia.component.scss']
+})
+export class FrequenciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
