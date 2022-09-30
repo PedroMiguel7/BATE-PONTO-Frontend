51fff30a3fa16614b4c4680108f15f2d9c64674b
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-09-30 08:44:19 -0300

    :sparkles: feat: filtros na Página de Pessoas

--- src/app/pessoas/tabela-pessoas/tabela-pessoas.component.ts
+++ src/app/pessoas/tabela-pessoas/tabela-pessoas.component.ts
@@ -13,15 +13,37 @@ export class TabelaPessoasComponent implements OnInit {
 
   orderBy = "id"
   order = "asc"
-
+  loading = false
   dataList: Pessoa[] = []
-  cabecalhos: string[] = []
+
+  getPessoas(){
+    this.loading = true
+    this.PessoasService.getPessoas(this.orderBy, this.order)
+    .then((res: any) => {
+      this.dataList = res.data;
+      this.loading = false
+    })
+  }
+
+  changeFiltro(orderBy: number, order: number) {
+    switch (orderBy) {
+      case 1: this.orderBy = "id"; break;
+      case 2: this.orderBy = "nome"; break;
+      case 3: this.orderBy = "email"; break;
+      case 4: this.orderBy = "tipo"; break;
+      case 5: this.orderBy = "created_at"; break;
+      default: break;
+    }
+
+    switch (order) {
+      case 1: this.order = "asc"; break;
+      case 2: this.order = "desc"; break;
+      default: break;
+    }
+    this.getPessoas()
+  }
+
   ngOnInit(): void {
-    this.PessoasService
-                    .getPessoas(this.orderBy, this.order)
-                    .then((res: any) => {
-                      this.dataList = res.data;
-                      this.cabecalhos = Object.keys(res.data[0])
-                    })
+    this.getPessoas()
   }
 }
