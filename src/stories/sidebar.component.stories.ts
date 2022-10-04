e478fc52b88f6d52aba1b37e974cf4452beb41d9
larissacard <larissak.cardosom@gmail.com>
2022-10-04 11:37:21 -0300

    :wrench: chore: install and configure Storybook

---
+++ src/stories/sidebar.component.stories.ts
@@ -0,0 +1,12 @@
+import { Meta } from '@storybook/angular'
+import { SidebarUserComumComponent } from "src/app/components/sidebar-user-comum/sidebar-user-comum.component";
+
+export default {
+    title: 'Sidebar',
+    component: SidebarUserComumComponent
+    
+};
+
+export const Default = () => ({
+    component: SidebarUserComumComponent
+})
