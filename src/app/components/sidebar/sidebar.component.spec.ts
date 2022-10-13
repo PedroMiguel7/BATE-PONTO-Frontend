5d9cb048279f1b7c30e217f13ad132beb06cbf11
larissacard <larissak.cardosom@gmail.com>
2022-10-13 10:59:19 -0300

    :test_tube: test: trying first test

--- src/app/components/sidebar/sidebar.component.spec.ts
+++ src/app/components/sidebar/sidebar.component.spec.ts
@@ -1,22 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { SidebarComponent } from './sidebar.component';
+import { SidebarComponent } from "./sidebar.component";
 
 describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SidebarComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(SidebarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('it should return 1', () => {
+    const sut = new SidebarComponent();
+    expect(sut.fazLogout()).toBeUndefined();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+})
